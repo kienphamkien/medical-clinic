928c3b0c1efa73ef1fd510ca7fbe95c18eb8d981
bushvatoi <kienpham16496@gmail.com>
2021-04-04 13:00:16 -0500

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -33,7 +33,7 @@ exports.register = (req, res) => {
         let hashedPassword = await bcryptjs.hash(Password, 8);
         console.log(hashedPassword);
 
-        db.query('INSERT INTO PATIENT set? ', {Fname: Fname, Minit: Minit, Lname: Lname, DOB: DOB, Address: Address, PhoneNumber: PhoneNumber, Email: Email, Password: Password}, (error, results) =>{
+        db.query('INSERT INTO PATIENT set? ', {Fname: Fname, Minit: Minit, Lname: Lname, DOB: DOB, Address: Address, PhoneNumber: PhoneNumber, Email: Email, Pass: Password}, (error, results) =>{
             if (error) {
                 console.log(error);
             } else {
